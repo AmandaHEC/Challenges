486ef2724665378e3c670808c77181c356a63d61
AmandaHEC <93448955+AmandaHEC@users.noreply.github.com>
2022-04-26 12:44:33 -0500

    FirstReverse

--- src/FirstReverse.java
+++ src/FirstReverse.java
@@ -1,14 +1,10 @@
-package Challenges;
-
 public class FirstReverse {
     public static void main(String[] args) {
-
-        function FirstReverse (str) {
-
-        return str.split('').reverse().join('');
-
+        function FirstReverse(str) {
+            return str.split('').reverse().join('');
         }
     }
+
 // keep this function call here
 console.log(FirstReverse(readline()));
 }
