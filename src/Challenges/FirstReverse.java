fd56e3639bf5dbcd17ed7ace17f5fbd748819b25
AmandaHEC <93448955+AmandaHEC@users.noreply.github.com>
2022-04-26 12:41:21 -0500

    firstReverse

---
+++ src/Challenges/FirstReverse.java
@@ -0,0 +1,14 @@
+package Challenges;
+
+public class FirstReverse {
+    public static void main(String[] args) {
+
+        function FirstReverse (str) {
+
+        return str.split('').reverse().join('');
+
+        }
+    }
+// keep this function call here
+console.log(FirstReverse(readline()));
+}
